435007f49d9058b14c0d16347bf35e8ac2637aa2
mimosaya <mimosaya@gmail.com>
2019-05-22 10:22:48 +0800

    lession 1 for tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from  './hero';
+
+export const HEROES : Hero[] = [
+    {id: 11, name: 'A11a'},
+    {id: 12, name: 'B12b'},
+    {id: 13, name: 'C13c'},
+    {id: 14, name: 'D14d'},
+    {id: 15, name: 'E15e'},
+    {id: 16, name: 'F16f'},
+    {id: 17, name: 'G17g'},
+    {id: 18, name: 'H18h'},
+    {id: 19, name: 'I19i'},
+    {id: 20, name: 'J20j'}
+];
